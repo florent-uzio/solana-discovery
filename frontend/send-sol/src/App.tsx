ef39b436bfd8204ad7f2efb66c9c61818c24a537
Florent Uzio <fuzio@ripple.com>
2024-03-01 13:14:41 +0400

    send sol working

--- frontend/send-sol/src/App.tsx
+++ frontend/send-sol/src/App.tsx
@@ -4,7 +4,7 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
 import { clusterApiUrl } from "@solana/web3.js"
 import { useMemo } from "react"
-import { Banner } from "./components"
+import { Banner, SendSolForm } from "./components"
 import { Balance } from "./components/balance"
 import("@solana/wallet-adapter-react-ui/styles.css")
 
@@ -29,6 +29,7 @@ function App() {
               </Flex>
 
               <Balance />
+              <SendSolForm />
             </Flex>
           </WalletModalProvider>
         </WalletProvider>
--- frontend/send-sol/src/components/send-sol-form.tsx
+++ frontend/send-sol/src/components/send-sol-form.tsx
@@ -1,13 +1,57 @@
-import { FormControl, FormHelperText, FormLabel, Input } from "@chakra-ui/react"
+import { Button, FormControl, FormHelperText, FormLabel, Input } from "@chakra-ui/react"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js"
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type Inputs = {
+  amount: number
+  destination: string
+}
 
 export const SendSolForm = () => {
+  const { register, handleSubmit } = useForm<Inputs>()
+  const { publicKey, sendTransaction } = useWallet()
+  const { connection } = useConnection()
+
+  const onSubmitHandler: SubmitHandler<Inputs> = async ({ amount, destination }) => {
+    alert(JSON.stringify({ amount, destination }, null, 2))
+
+    if (!publicKey) {
+      return
+    }
+
+    const transaction = new Transaction()
+    const recipientPubKey = new PublicKey(destination)
+
+    const sendSolInstruction = SystemProgram.transfer({
+      fromPubkey: publicKey,
+      toPubkey: recipientPubKey,
+      lamports: LAMPORTS_PER_SOL * amount,
+    })
+
+    transaction.add(sendSolInstruction)
+
+    const sig = await sendTransaction(transaction, connection)
+    console.log("Signature: ", sig)
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
       <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input />
-        <FormHelperText>We'll never share your email.</FormHelperText>
+        <FormLabel>Amount</FormLabel>
+        <Input minW="400px" {...register("amount")} />
+        <FormHelperText>Amount of SOL to send</FormHelperText>
       </FormControl>
-    </>
+
+      <FormControl my="5">
+        <FormLabel>Destination</FormLabel>
+        <Input minW="500px" {...register("destination")} />
+        <FormHelperText>Solana Destination Address</FormHelperText>
+      </FormControl>
+
+      <Button type="submit" colorScheme="blue" ml="100%">
+        Send
+      </Button>
+    </form>
   )
 }
